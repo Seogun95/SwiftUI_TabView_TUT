2fdf104f01ac2da045e1830ccd441583f82cb43b
SeonJungKim <ksj083895@gmail.com>
2021-03-08 02:33:04 +0500

    버튼을 탭 할 때마다 뷰 전환

--- SwiftUI_TabView_TUT/ContentView.swift
+++ SwiftUI_TabView_TUT/ContentView.swift
@@ -8,31 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 0
     var body: some View {
-        TabView {
-            GreenTapView()
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("홈으로")
-                }
-            redTabView()
-                .tabItem {
-                    Image(systemName: "person")
-                    Text("사용자")
-                }
-            
-            blueTabView()
-                .tabItem {
-                    Image(systemName: "bag")
-                    Text("장바구니")
-                }
+        ZStack(alignment: .topTrailing) {
+            TabView(selection: $selection) {
+                Text("첫번째 뷰")
+                    .font(.system(size: 30))
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("홈으로")
+                    }
+                    .tag(0)
                 
+                Text("두번째 뷰")
+                    .font(.system(size: 30))
+                    .tabItem {
+                        Image(systemName: "bookmark.circle.fill")
+                        Text("북마크")
+                    }
+                    .tag(1)
+                
+                Text("세번째 뷰")
+                    .font(.system(size: 30))
+                    .tabItem {
+                        Image(systemName: "video.circle.fill")
+                        Text("비디오")
+                    }
+                    .tag(2)
+            }
+            .accentColor(.red)
+            .onAppear() {
+                UITabBar.appearance().barTintColor = .white
+            }
+            Button(action : {
+                selection = (selection + 1) % 3
+            }) {
+                Text("다음")
+                    .font(.system(.headline))
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .padding()
+            }
         }
-        .onAppear() {
-            UITabBar.appearance().barTintColor = .white
-            
-        }
-        .accentColor(.red)
     }
 }
 struct ContentView_Previews: PreviewProvider {
