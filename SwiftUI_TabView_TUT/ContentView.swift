d9595971b3908d5d48cae9b4c70c3779a164a159
SeonJungKim <ksj083895@gmail.com>
2021-03-07 16:54:57 +0500

    Initial Commit

---
+++ SwiftUI_TabView_TUT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_TabView_TUT
+//
+//  Created by 김선중 on 2021/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_TabView_TUT/SwiftUI_TabView_TUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TabView_TUTApp.swift
+//  SwiftUI_TabView_TUT
+//
+//  Created by 김선중 on 2021/03/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TabView_TUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
